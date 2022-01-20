d3baf7abb0fd2cb0a0f67509d6599c1e1517c798
Mauriel Jhoel Narvaez Gutierrez <maurieljng@gmail.com>
2022-01-19 20:24:40 -0600

    Add project files.

---
+++ CrudAPI/DBContext/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudAPI
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    }
+}
