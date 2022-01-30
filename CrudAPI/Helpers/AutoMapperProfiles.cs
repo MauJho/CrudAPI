4ac1804a14bc265cb0367647c82156ba0b451fe5
Mauriel Jhoel Narvaez Gutierrez <maurieljng@gmail.com>
2022-01-30 16:26:07 -0600

    Add the JWT for authentication and the address to the DB server

---
+++ CrudAPI/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace CrudAPI.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+
+        }
+    }
+}
